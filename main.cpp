d222f11345abba77e930d325bebfd1e575b365e6
Shakeel Shafiq <shakeel.shafique@gmail.com>
2017-01-22 22:45:46 +0500

    Call after including

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "counting-dna-nucleotides.h";
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    basic_string<char> sequence = argv[1];
+
+    count_dna_nucleotides(sequence);
+
+    return 0;
+}
